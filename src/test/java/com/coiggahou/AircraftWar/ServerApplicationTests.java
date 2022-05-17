9c595b66f9f3b996fb2ef477c871538dbfd5514c
ccccccjh <72149771+Coiggahou2002@users.noreply.github.com>
2022-05-17 23:35:38 +0800

    init 端口号 12333

---
+++ src/test/java/com/coiggahou/AircraftWar/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coiggahou.AircraftWar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
